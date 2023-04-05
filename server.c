b19bd6caaed763b9b70ec4b18d3cd0b967929041
Eduardo Aguiar <64619105+Aguiar08@users.noreply.github.com>
2023-04-05 09:22:46 -0300

    Update server.c

--- server.c
+++ server.c
@@ -18,7 +18,37 @@
 
 
 void create_profile() {
-
+    
+    char email[50], nome[50], sobrenome[50], residencia[50], formacao[50], habilidades[100];
+    int ano_formatura;
+    printf("Digite o seu email: ");
+    scanf("%s", email);
+    printf("Digite o seu nome: ");
+    scanf("%s", nome);
+    printf("Digite o seu sobrenome: ");
+    scanf("%s", sobrenome);
+    printf("Digite a sua residencia: ");
+    scanf("%s", residencia);
+    printf("Digite a sua formacao academica: ");
+    scanf("%s", formacao);
+    printf("Digite o ano de sua formatura: ");
+    scanf("%d", &ano_formatura);
+    printf("Digite suas habilidades: ");
+    scanf("%s", habilidades);
+    FILE *file = fopen(email, "w");
+    if (file == NULL) {
+        printf("Erro ao criar arquivo!\n");
+        return 1;
+    }
+    fprintf(file, "Email: %s\n", email);
+    fprintf(file, "Nome: %s Sobrenome: %s\n", nome, sobrenome);
+    fprintf(file, "Residencia: %s\n", residencia);
+    fprintf(file, "Formacao Academica: %s\n", formacao);
+    fprintf(file, "Ano de Formatura: %d\n", ano_formatura);
+    fprintf(file, "Habilidades: %s\n", habilidades);
+    fclose(file);
+    printf("Perfil criado e salvo em perfil.txt com sucesso!\n");
+    return 0;
 }
 
 void get_profile() {
@@ -144,7 +174,7 @@ int main(void) {
 
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!!!!", 13, 0) == -1)
+            if (send(new_fd, "Hello, world!", 13, 0) == -1)
                 perror("send");
             close(new_fd);
             exit(0);
